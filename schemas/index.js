d498fd627254cb159968e50495f609fb2665a140
이총명 <dlchdaud1327@gmail.com>
2021-12-25 20:37:57 +0900

    [task] - db 정보 업데이트

--- schemas/index.js
+++ schemas/index.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 
 const connect = () => {
     mongoose
-
-        //  test:test id/pws @ ip
-
-        .connect("mongodb://localhost:27017/nodejjang", {
+        // .connect("mongodb://localhost:27017/nodejjang", {
+        .connect(`mongodb://${process.env.MONGO_URL}/${process.env.MONGO_DB}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-
+            user: process.env.MONGO_USER,
+		    pass: process.env.MONGO_PASS
         })
         .catch((err) => console.log(err));
 };
